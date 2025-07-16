7ebcaf1b6e285102a22626f742a034f526df4b8f
CHRISTOPHER SILKWORTH <crsilkworth@crsilkworth01.lan>
2025-07-16 15:25:09 +0900

    don't send value to output

--- utils/run_utils.py
+++ utils/run_utils.py
@@ -246,12 +246,7 @@ async def prepare_output(att, att_dict, output, with_db):
         if serialize_output:
             return serialize_output
 
-    if (
-        att_dict.get("gcs_stored", False)
-        or att_dict.get("connection", False)
-        or att_dict.get("model", False)
-    ):
-        del output["value"]
+    del output["value"]
 
     output["_local_rep"] = _local_rep
     output["_local_type"] = att_dict["_local_type"]
