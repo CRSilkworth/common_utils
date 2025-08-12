0a740ee1f3a0057033887ece66b965c10c122e52
CHRISTOPHER SILKWORTH <crsilkworth@crsilkworth01.lan>
2025-08-12 20:17:36 +0900

    debug prints

--- utils/function_utils.py
+++ utils/function_utils.py
@@ -59,6 +59,7 @@ def create_function(
         for key, value in global_vars.items():
             full_function_string += f"{key} = {repr(value)}\n"
         full_function_string = full_function_string + function_string
+        print(full_function_string)
         bytecode = compile(full_function_string, filename="<inline code>", mode="exec")
         exec_result = {}
         exec(bytecode, allowed_modules, exec_result)
