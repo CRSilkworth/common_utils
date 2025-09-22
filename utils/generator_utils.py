c3eb760cfdae1644f0387b6c544b08bfe16f5c50
CHRISTOPHER SILKWORTH <crsilkworth@crsilkworth01.lan>
2025-09-22 13:31:00 +0900

    Bump common_utils version to v0.8.213-dev

--- utils/generator_utils.py
+++ utils/generator_utils.py
@@ -1,9 +1,4 @@
-from typing import Text, Any, List, Iterator, Tuple, Dict, Optional
-from itertools import groupby
-from operator import itemgetter
-from utils.type_utils import Allowed
-from utils.serialize_utils import attempt_deserialize
-from utils.gcp_utils import read_from_gcs_signed_urls
+from typing import List, Iterator, Tuple, Dict, Optional
 
 
 def strict_zip(*gens):
@@ -27,34 +22,6 @@ def strict_zip(*gens):
             )
 
         yield tuple(values)
-
-
-def generator_from_urls(signed_urls: List[Text], value_type: Any = Allowed):
-    value = read_from_gcs_signed_urls(signed_urls) if signed_urls else []
-
-    for file_content in value:
-        file_content, output, _ = attempt_deserialize(file_content, value_type)
-        if output:
-            raise ValueError(
-                "failed to deserialize file from generator: "
-                f"{output['stderr_output']}"
-            )
-        for item in file_content:
-            yield item
-
-
-def sim_values_generator(sim_value_gen, chunked: bool = False):
-    """
-    Convert a generator with values of the form (sim_parms, value)  to a generator
-    of a generator (if chunked) where the outer generator  goes over the sim_params
-    and the inner over the values with the same sim_params
-    """
-    if chunked:
-        for key, group in groupby(sim_value_gen, key=itemgetter(0)):
-            yield (value for _, value in group)
-    else:
-        for key, value in sim_value_gen:
-            yield value
 
 
 def merge_generators(generators):
