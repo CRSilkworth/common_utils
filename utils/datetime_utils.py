607cd1f7d78c6f09608cde33a5463028555e6120
CHRISTOPHER SILKWORTH <crsilkworth@crsilkworth01.lan>
2025-10-09 08:23:27 +0900

    Bump common_utils version to v0.8.296-dev

--- utils/datetime_utils.py
+++ utils/datetime_utils.py
@@ -1,17 +1,21 @@
-import datetime as dt
+import datetime
 
-EPOCH = dt.datetime(1970, 1, 1)
+EPOCH = datetime.datetime(1970, 1, 1)
 
-dt_min = dt.datetime.min
-dt_max = dt.datetime.max
+datetime_min = datetime.datetime.min
+datetime_max = datetime.datetime.max
 
 
-def datetime_to_float(d: dt.datetime) -> float:
+def datetime_to_float(d: datetime.datetime) -> float:
     """Convert datetime to float seconds since epoch, works for min/max."""
     delta = d - EPOCH
     return delta.total_seconds()
 
 
-def float_to_datetime(value: float) -> dt.datetime:
+def float_to_datetime(value: float) -> datetime.datetime:
     """Convert float seconds since epoch back to datetime."""
-    return EPOCH + dt.timedelta(seconds=value)
+    return EPOCH + datetime.timedelta(seconds=value)
+
+
+def to_micro(dt):
+    return dt.replace(microsecond=(dt.microsecond // 1000) * 1000)
